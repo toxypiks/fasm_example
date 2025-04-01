a43ac6d8d758daba73d252c91a8e793c04c5a953
toxypiks <HannaHinkelbein89@gmx.de>
2025-04-01 13:41:21 +0200

    Fasm is an interpreted language

--- hello.asm
+++ hello.asm
@@ -1,10 +1,14 @@
 format ELF64 executable
 
+SYS_WRITE = 1
+SYS_EXIT = 60
+X = 1
+
 macro write fd, buf, count
 {
     ; 1 = number of syscall write
     ; rax register syscall, rdi, rsi and rdx register for parameters
-    mov rax, 1
+    mov rax, SYS_WRITE
     ; standard output 1
     mov rdi, fd
     ; message to write
@@ -17,18 +21,23 @@ macro write fd, buf, count
 macro exit code
 {
     ; second syscall to exit
-    mov rax, 60
+    mov rax, SYS_EXIT
     ; mov rdi register for exit code (0)
     mov rdi, code
     syscall
 }
 
+X = X + 2
+
 segment readable executable
 entry main
 main:
+repeat X
     write 1, msg, msg_len
+end repeat
     exit 0
 
 segment readable writeable
-msg db "Hello, World!", 10
+msg db "Hello World!", 10
+; 10 = ASCII value for LINEFEED
 msg_len = $ - msg
