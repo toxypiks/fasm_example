d85930ba63b87acb0ddd06eec1870b036972f6a5
toxypiks <HannaHinkelbein89@gmx.de>
2025-04-03 12:45:01 +0200

    Write ERROR in STDERROR when socketId is less then 0

--- website.asm
+++ website.asm
@@ -7,6 +7,9 @@ SYS_SOCKET = 41
 AF_INET = 2
 SOCK_STREAM = 1
 
+STDOUT = 1
+STDERR = 2
+
 macro write fd, buf, count
 {
     mov rax, SYS_WRITE
@@ -37,13 +40,22 @@ segment readable executable
 entry main
 main:
 
-    write 1, start, start_len
-    socket AF_INET, SOCK_STREAM, 0
+    write STDOUT, start, start_len
+    ;; socket AF_INET, SOCK_STREAM, 0
+    socket 69, 420, 0
+    cmp rax, 0 ;; compares value of rax to 0
+    jl error ;; jump if its less then 0 to error
     mov dword [sockfd], eax ;; move file descriptor of socket from eax register to sockfd
-    ;; dword indicates a 32-bit write
+    ;; dword indicates a 32-bit write to prevent overwriting
     exit 0
+
+error:
+    write STDERR, error_msg, error_msg_len
+    exit 1
 
 segment readable writeable
 sockfd dd 0
 start db "Hello Web Server!", 10
 start_len = $ - start
+error_msg db "ERROR!", 10
+error_msg_len = $ - error
