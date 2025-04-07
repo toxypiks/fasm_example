d22f5e55c4f56c70a45b37d465274da2885b67a7
toxypiks <HannaHinkelbein89@gmx.de>
2025-04-07 09:58:23 +0200

    Implement loop

--- main.asm
+++ main.asm
@@ -1,20 +1,27 @@
 format ELF64 executable
 
-segment readable executable
-
+entry main
 main:
-   mov rax, 1
-   mov rdi, 1
-   mov rsi, my_msg
-   mov rdx, 39
-   syscall
+   ;; int3 sets entry point for debugging
+   mov r15, 10
+.again:
+   cmp r15, 0
+   jle .over ;; jump when less or equal to 0
+       mov rax, 1
+       mov rdi, 1
+       mov rsi, my_msg
+       mov rdx, my_msg_len
+       syscall
+       dec r15
+   jmp .again
 
+.over:
    mov rax, 60
    mov rdi, 0
    syscall
 
-segment readable writeable
 msg db "Hello World!", 10
 msg_len = $ - msg
-my_msg:
-file "./message.txt"
+
+my_msg: file "./message.txt"
+my_msg_len = $ - my_msg
